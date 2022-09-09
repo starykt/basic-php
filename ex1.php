b4fa0bc7c017fad6bfea809f5a61b88679368680
unknown <starykt.gabs@gmail.com>
2022-09-09 16:49:02 -0300

    [FIX] enunciados e validação

--- ex1.php
+++ ex1.php
@@ -10,6 +10,9 @@
 </head>
 
 <body>
+  <!-- Enunciado do exercício -->
+  <!-- Faça um programa que receba um valor pago, um segundo valor que é o preço do produto e retorne o troco a ser dado. -->
+  
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   <div class="container">
     <div class="row">
--- ex2.php
+++ ex2.php
@@ -10,21 +10,25 @@
 </head>
 
 <body>
+  <!-- Enunciado do exercício -->
+  <!-- Faça um programa que receba o valor do quilo de um produto e a quantidade de quilos do produto consumida,
+    calculando o valor final a ser pago.  -->
+
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
   <div class="container">
     <div class="row">
       <div class="col-6">
-        <h1>Compra</h1>
+        <h1>Compra por KG</h1>
         <div class="card">
           <div class="card-body">
             <form action="" method="get">
               <div class="form-group">
                 <label for="title">Valor do quilo (R$):</label>
-                <input class="form-control" type="number" name="priceK" id="priceK" placeholder="Valor do quilo">
+                <input class="form-control" type="number" name="priceK" id="priceK" placeholder="Valor do quilo" required>
               </div>
               <div class="form-group">
                 <label for="price">Total de quilos comprados:</label>
-                <input class="form-control" type="number" name="buyK" id="buyK" placeholder="Total de quilos">
+                <input class="form-control" type="number" name="buyK" id="buyK" placeholder="Total de quilos" required>
               </div> <br>
               <div class="form-group text-center">
                 <button type="submit" class="btn btn-outline-dark">Calcular</button>
@@ -33,14 +37,16 @@
               <?php
 
               if (isset($_GET['priceK']) and (isset($_GET['buyK']))) {
-
                 $precoquilo = $_GET['priceK'];
                 $totalquilos = $_GET['buyK'];
-                $total = $precoquilo * $totalquilos;
 
-                echo "Total do produto: (R$)$total";
+                if (($precoquilo < 0) || ($totalquilos < 0)) {
+                  echo "Digite apenas valores positivos.";
+                } else {
+                  $total = $precoquilo * $totalquilos;
+                  echo "Total da compra do produto: (R$)$total";
+                }
               }
-
               ?>
             </form>
           </div>
